57a1d751501db2dd0540906c68bd40154004d475
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2023-10-19 15:38:39 +0530

    Login form and student detail form has been created succesfuly

--- app.js
+++ app.js
@@ -6,19 +6,37 @@ import mongoose from "mongoose";
 // Variable Declaration 
 const app = express();
 const port = 3000;
+const username = "tejasvi";
+const password = 1234;
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+
 // Connect to MongoDB database using Mongoose
-mongoose.connect('mongodb://localhost/library');
+// mongoose.connect('mongodb://localhost/library');
 
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
 })
 
+app.get("/login-btn", (req, res) => {
+    res.render("index.ejs")
+})
 
+app.post("/login", (req, res) => {
+    const formusername = req.body["username"];
+    const formpassword = req.body["password"];
 
+    if (formpassword == password && formusername == username) {
+        res.render("admin.ejs");
+    }
+    else (
+        res.render("login-error.ejs")
+    )
+})
 
 
 
