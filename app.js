dfe01ed5c4f053b657f31eade7352ab1a0e1090d
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2023-10-19 17:22:01 +0530

    database and collection created and data insertiontion to collection tested succesfully

--- app.js
+++ app.js
@@ -15,7 +15,37 @@ app.use(express.static("public"));
 
 
 // Connect to MongoDB database using Mongoose
-// mongoose.connect('mongodb://localhost/library');
+
+mongoose.connect("mongodb://127.0.0.1:27017/libraryDB")
+    .then(() => {
+        console.log("Database Created succesfully");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+// Create schema for student detail
+
+const studentSchema = new mongoose.Schema({
+    firstname: String,
+    lastname: String,
+    email: String,
+    mobilenumber: Number,
+    vill: String,
+    city: String,
+    state: String,
+    pincode: Number,
+    dateofenrollment: Date,
+    amountpaid: {
+        type: Number,
+        min: 300,
+        max: 1000
+    },
+    shift: String
+
+});
+
+const Student = mongoose.model("Student", studentSchema);
 
 
 app.get("/", (req, res) => {
@@ -36,7 +66,46 @@ app.post("/login", (req, res) => {
     else (
         res.render("login-error.ejs")
     )
-})
+});
+
+app.post("/admin", (req, res) => {
+
+    const firstname = req.body["first-name"];
+    const lastname = req.body["last-name"];
+    const email = req.body["email"];
+    const mobilenumber = req.body["mobile-number"];
+    const vill = req.body["vill"];
+    const city = req.body["city"];
+    const state = req.body["state"];
+    const pincode = req.body["pincode"];
+    const enrollment = req.body["enrollment-date"];
+    const amount = req.body["amount-paid"];
+    const shift = req.body["shift"];
+
+    const student = new Student({
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        mobilenumber: mobilenumber,
+        vill: vill,
+        city: city,
+        state: state,
+        pincode: pincode,
+        dateofenrollment: enrollment,
+        amountpaid: amount,
+        shift: shift
+
+    });
+    student.save()
+        .then(() => {
+            console.log("Data saved succesfully in to the collection");
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+
+    res.redirect("/")
+});
 
 
 
